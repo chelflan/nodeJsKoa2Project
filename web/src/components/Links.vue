0bb6c2d4d6cb6f90f446d55c0e6a1cc387abdd3e
chelflan <284369904@qq.com>
2019-10-22 00:39:34 +0800

    link change

--- web/src/components/Links.vue
+++ web/src/components/Links.vue
@@ -18,10 +18,9 @@
     data() {
       return {
         linkList: [
-          {name: 'Github', href: 'https://github.com/liangfengbo'},
-          {name: '慕课网', href: 'https://www.imooc.com/u/3957223'},
-          {name: '掘金', href: 'https://juejin.im/user/58c2981d8ac2470720f75bb9'},
-          {name: 'Coding.net', href: 'https://dev.tencent.com/u/liangfengbo'}
+          {name: 'Github', href: 'https://github.com/chelflan'},
+          {name: 'CSDN', href: 'https://blog.csdn.net/qq_31001889'},
+          {name: '每日一花 Dailyflower_', href: 'http://www.dailyflower.club'},
         ]
       }
     },
