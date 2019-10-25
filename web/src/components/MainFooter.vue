5205d6010efd9d388c0538c119944faf4da5fc7c
成都富顿科技－张庆 <zhangqing@cdfortis.com>
2019-10-25 10:06:02 +0800

    前台的页脚显示

--- web/src/components/MainFooter.vue
+++ web/src/components/MainFooter.vue
@@ -1,8 +1,8 @@
 <template>
   <section class="Footer-wrap">
     <footer class="footer-inner">
-      <p>www.boblog.com - 波博客 Copyright&copy; Author梁凤波 itbo@163.com</p>
-      <p><a href="http://www.miit.gov.cn" target="_blank">粤ICP备18001135号-3</a></p>
+      <p>www.chelflan.cn - 赤f蓝 Copyright&copy; Author Chelflan</p>
+      <p><a href="http://www.miit.gov.cn" target="_blank">蜀ICP备19027522号-2</a></p>
     </footer>
   </section>
 </template>
